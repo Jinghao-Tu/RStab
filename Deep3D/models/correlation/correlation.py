3f11e8bbeb3e2369c4e2a00308e1797bace201ce
Jinghao Tu <jinghaotu213@jht213.com>
2025-06-10 16:59:48 +0800

    refactor: replace cupy.cuda.compile_with_cache with cupy.RawKernel in cupy_launch function

--- Deep3D/models/correlation/correlation.py
+++ Deep3D/models/correlation/correlation.py
@@ -139,7 +139,7 @@ def cupy_kernel(strFunction, objectVariables):
 
 @cupy.memoize(for_each_device=True)
 def cupy_launch(strFunction, strKernel):
-	return cupy.cuda.compile_with_cache(strKernel).get_function(strFunction)
+	return cupy.RawKernel(strKernel, strFunction)
 # end
 
 class FunctionCorrelation(torch.autograd.Function):
